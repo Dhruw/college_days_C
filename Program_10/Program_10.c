4aed78c67caec220e9ded63960138b51b4b8cfb9
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2018-07-26 14:57:19 +0530

    Added a program to demonstrate bubble sorting

---
+++ Program_10/Program_10.c
@@ -0,0 +1,56 @@
+/*
+ * BUBBLE SORT
+ *	
+ * Author: Divyanshu Dhruw
+ * Email: divyanshudhruwcsit@gmail.com
+ * Date: 26 July 2018
+ *
+ * @ A program to demonstrate the mist basic sorting algorith - Bubble Sort
+ * @program prompts for 5 integers
+ * @prints the numbers in ascending order
+ */
+
+/*
+ * THEORY:
+ * 
+ * The algorithm compares the adjacent numbers and swaps them accordingly
+ * 
+ * Worst Case Complexity: O(n^2)
+ * Average Case Complexity: O(n^2)
+ * Best Case Complexith O(n)
+ * 
+ * 
+ */
+
+
+#include<stdio.h>
+
+int main(void){
+	int series[5];
+	int temp;
+
+	printf("Enter five numbers: \n");
+
+	for(int i = 0; i < 5; i++)
+		scanf("%d", &series[i]);
+	
+	for(int i = 0; i < 4; i++)
+	{
+		for(int j = 0; j < 4; j++)
+		{
+			if( series[j] > series[j+1] )
+			{
+				temp = series[j];
+				series[j] = series[j+1];
+				series[j+1] = temp;
+			}
+		}
+	}
+
+	printf("The sorted sequence is : \n");
+
+	for(int i = 0; i < 5; i++)
+		printf("%d\n", series[i]);
+
+	return 0;
+}
